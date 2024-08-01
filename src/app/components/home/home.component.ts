893e32676d3a153ceb29743a5942b7ff2ebf4194
Matheus Batista <mbatista.sarti@gmail.com>
2024-08-01 01:13:14 -0300

    :sparkles: finish home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,12 @@
-import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
-import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SidebarModule, ButtonModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class HomeComponent {
-  isSidebarVisible = false;
-}
+export class HomeComponent {}
