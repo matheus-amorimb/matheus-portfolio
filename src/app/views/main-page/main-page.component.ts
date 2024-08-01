a397f772fade509fc66e656aa897a066477eaf49
Matheus Batista <mbatista.sarti@gmail.com>
2024-08-01 17:00:09 -0300

    :sparkles: projects component

--- src/app/views/main-page/main-page.component.ts
+++ src/app/views/main-page/main-page.component.ts
@@ -2,11 +2,12 @@ import { AboutComponent } from './../../components/about/about.component';
 import { HomeComponent } from './../../components/home/home.component';
 import { HeaderComponent } from './../../components/header/header.component';
 import { Component } from '@angular/core';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent, AboutComponent],
+  imports: [HeaderComponent, HomeComponent, AboutComponent, ProjectsComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
