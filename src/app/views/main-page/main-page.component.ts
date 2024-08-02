0cb6abc58513bd3224c2425555c1ffb9041320da
Matheus Batista <mbatista.sarti@gmail.com>
2024-08-01 23:48:47 -0300

    :sparkles: wrap first version of website

--- src/app/views/main-page/main-page.component.ts
+++ src/app/views/main-page/main-page.component.ts
@@ -3,11 +3,18 @@ import { HomeComponent } from './../../components/home/home.component';
 import { HeaderComponent } from './../../components/header/header.component';
 import { Component } from '@angular/core';
 import { ProjectsComponent } from '../../components/projects/projects.component';
+import { ContactComponent } from '../../components/contact/contact.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent, AboutComponent, ProjectsComponent],
+  imports: [
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
